e44672e2bb68d9cddce40299a7184ae26c83faed
TareqSaleem <tareqy2003@hotmail.com>
2016-11-09 13:02:13 +0100

    Singleproduct + Browsproduct
    Singleproduct + Browsproduct

---
+++ WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Singleproduct()
+        {
+            ViewBag.Message = "Your product page.";
+
+            return View();
+        }
+        public ActionResult Browseproduct()
+        {
+            ViewBag.Message = "Your browseproduct page.";
+
+            return View();
+        }
+    }
+}
