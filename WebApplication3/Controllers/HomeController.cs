c1b77f84be4239a1e56ad50bc0430b28abd646fd
TareqSaleem <tareqy2003@hotmail.com>
2016-11-10 01:53:31 +0100

    Update 1.1

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace WebApplication3.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About pagina";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact pagina";
 
             return View();
         }
