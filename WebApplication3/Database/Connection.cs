36dcee5cec38d31a72d8bee52a50d829eaa4e445
TareqSaleem <tareqy2003@hotmail.com>
2016-12-01 14:45:51 +0100

    Connection en program classes

---
+++ WebApplication3/Database/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication3.Database
+{
+    class Connection
+    {
+        public static MySqlConnection connection;
+        static string server;
+        static string database;
+        static string uid;
+        static string password;
+
+        public static MySqlConnection Initialize()
+        {
+            server = "145.24.222.102";
+            database = "sys";
+            uid = "root";
+            password = "Project56";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "Port = 3306;" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
---
+++ WebApplication3/Database/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace WebApplication3.Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = Connection.Initialize();
+            conn.Open();
+            var cm = new MySqlCommand("SELECT COUNT(*) from user", conn);
+            cm.Prepare();
+            var r = cm.ExecuteReader();
+            while (r.Read())
+            {
+                uId = Int32.Parse(r[0].ToString()) + 1;
+            }
+            var cmd = new MySqlCommand("INSERT INTO user (uId, uPassword, uName, uSurname, uEmail) VALUES (@val1,@val3,@val4,@val5,@val6)", Connection.connection);
+            cmd.Parameters.AddWithValue("@val1", uId);
+            cmd.Parameters.AddWithValue("@val3", password);
+            cmd.Parameters.AddWithValue("@val4", name);
+            cmd.Parameters.AddWithValue("@val5", surname);
+            cmd.Parameters.AddWithValue("@val6", email);
+            cmd.Prepare(); cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
